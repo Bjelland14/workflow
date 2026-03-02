c7920d94e582625470ccca4dea1fe8d4e4b8b889
Bjelland14 <lloydbjelland@gmail.com>
2026-03-02 13:30:30 +0100

    Format project with Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,18 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { 
-    files: ["**/*.{js,mjs,cjs}"],
-     plugins: { js }, 
-     extends: ["js/recommended"], 
-     languageOptions: { 
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
       globals: {
-        ...globals.browser, 
-        ...globals.node, 
+        ...globals.browser,
+        ...globals.node,
         ...globals.vitest
-      } 
-    },
+      }
+    }
   }
 ]);
--- js/utils/validation.js
+++ js/utils/validation.js
@@ -11,7 +11,7 @@ export function validateLoginForm(email, password) {
   const errors = [];
 
   if (!validateEmail(email)) {
-    errors.push("Please enter a noroff.no or stud.noroff.no email address.");
+    errors.push('Please enter a noroff.no or stud.noroff.no email address.');
   }
 
   if (!validatePassword(password)) {
@@ -20,6 +20,6 @@ export function validateLoginForm(email, password) {
 
   return {
     isValid: errors.length === 0,
-    errors: errors,
+    errors: errors
   };
 }
